69893f9289d0d944c1e496959b98adff0a47648b
ty <ty@551ac25d-1691-46f3-960b-ff54f7a9b640>
2010-11-23 17:08:40 +0000

    ADD - BuildProjectButton(), buildproject(), runbashscript() methods for admin tools

--- sitesprocket/code/SiteSprocketAdmin.php
+++ sitesprocket/code/SiteSprocketAdmin.php
@@ -50,7 +50,8 @@ class SiteSprocketAdmin extends LeftAndMain implements PermissionProvider
 		'upload',
 		'nextpage',
 		'prevpage',
-		'updateperpage'
+		'updateperpage',
+		'buildproject' => 'SITESPROCKET_BACKEND'
 	);
 	
 	
@@ -756,6 +757,85 @@ class SiteSprocketAdmin extends LeftAndMain implements PermissionProvider
 		return $this->customise(array(
 			'Messages' => $project->Messages()
 		))->renderWith('Messages');
+	}
+	
+	/**
+	 * Run a bash script and spit out a simple web response and code (for AJAX)
+	 *
+	 * @return SS_HTTPResponse
+	 */	
+	protected function runbashscript($scriptPath, $paramArray) {
+	
+		// Set up the variables
+		//$scriptPath .= ' ';
+		$parameters = implode(' ',$paramArray);
+	
+		$descriptorspec = array(
+			0 => array("pipe","r"),
+			1 => array("pipe","w"),
+			2 => array("pipe","./error.log","a")
+		) ;
+		
+		// define current working directory where files would be stored
+		$cwd = './';
+		// open process /bin/sh
+		$process = proc_open('/bin/sh', $descriptorspec, $pipes, $cwd);
+		
+		if(!file_exists($scriptPath)) {
+			// we send back a success code for Bash failure, because the web request succeeded, the bash script failed
+			// would be better to send back a 500, but I can't seem to get jQuery's .ajaxError() working in ssp_admin.js
+			return new SS_HTTPResponse(sprintf('%s', 'SCRIPT NOT FOUND'),200);
+		}
+		
+		// Sanitize the script path
+		$scriptPath .= ' ';
+		
+		if (is_resource($process)) {
+			
+			// anatomy of $pipes: 0 => stdin, 1 => stdout, 2 => error log
+			fwrite($pipes[0], $scriptPath . $parameters);
+			fclose($pipes[0]);
+			
+			// print pipe output
+			$message = stream_get_contents($pipes[1]);
+			
+			// close pipe
+			fclose($pipes[1]);
+			
+			// all pipes must be closed before calling proc_close. 
+			// proc_close() to avoid deadlock
+			proc_close($process);
+			
+			return new SS_HTTPResponse(sprintf('%s', $message), 200);
+		}
+
+	}
+	
+	/**
+	 * A button for calling the build project bash script - see ssp_admin.js for AJAX implementation
+	 *
+	 * @return InlineFormAction
+	 */
+	public function BuildProjectButton() {
+		$a = new InlineFormAction('BuildProject','Build Project');
+		return $a;
+		
+	}
+	
+	/**
+	 * Builds a new project using a bash script
+	 *
+	 * @return SS_HTTPResponse
+	 */	
+	public function buildproject() {
+				
+		if(is_numeric($this->getRequest()->requestVar('id'))) {
+		
+			$project_id = $this->getRequest()->requestVar('id');
+			
+			return $this->runbashscript('/Users/tybarho/Sites/Scripts/check_mysqlDB_exist.sh', array($project_id, 'root'));
+		
+		}
 	}
 	
 		
